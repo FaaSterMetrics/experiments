18bc40eec86d2fc18367b92986ef8f60526cd093
Christoph Witzko <github@christophwitzko.com>
2020-07-01 13:53:15 +0200

    feat: basic implementation

--- experiments/iot/functions/getlightphasecalculation/index.js
+++ experiments/iot/functions/getlightphasecalculation/index.js
@@ -3,14 +3,19 @@ const lib = require('@faastermetrics/lib')
 /*
 bit order: 0b[red][yellow][green]
 {
-  lights: 0b000,
+  lights: ["red","green", "yellow"],
   blink: false
 }
 */
 
-module.exports = lib.serverless.rpcHandler(async (event, ctx) => {
-  return {
-    lights: 0b000,
-    blink: false
+module.exports = lib.serverless.rpcHandler(
+  { db: 'redis' },
+  async (event, ctx) => {
+    const light = await ctx.db.get('lightcalculation:light')
+    const blink = (await ctx.db.set('lightcalculation:blink')) || true
+    return {
+      lights: light,
+      blink
+    }
   }
-})
+)
